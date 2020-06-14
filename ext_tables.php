8d75a187af1848b577068605514090638249acb4
Sebastian Fischer <typo3@evoweb.de>
2020-06-14 15:23:37 +0200

    Fix dependencies and resources

--- ext_tables.php
+++ ext_tables.php
@@ -2,31 +2,26 @@
 
 defined('TYPO3_MODE') or die('Access denied.');
 
-/** @var \TYPO3\CMS\Core\Configuration\ExtensionConfiguration $extensionConfiguration */
-$extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-    \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
-);
-$showAdministrationModule = (bool)$extensionConfiguration->get('prometheus', 'showAdministrationModule');
-if ($showAdministrationModule == true) {
-    call_user_func(
-        function ($extKey) {
-            if (TYPO3_MODE === 'BE') {
-                \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-                    'Prometheus',
-                    'system',
-                    'prometheus',
-                    '',
-                    [
-                        \Mfc\Prometheus\Controller\Backend\PrometheusController::class => 'getGrafanaContent',
-                    ],
-                    [
-                        'access' => 'user,group',
-                        'icon' => 'EXT:' . $extKey . '/Resources/Public/Icon/Icon.svg',
-                        'labels' => 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_mod.xml',
-                    ]
-                );
-            }
-        },
-        'prometheus'
+call_user_func(function () {
+    /** @var \TYPO3\CMS\Core\Configuration\ExtensionConfiguration $extensionConfiguration */
+    $extensionConfiguration = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Configuration\ExtensionConfiguration::class
     );
-}
+    $showAdministrationModule = (bool)$extensionConfiguration->get('prometheus', 'showAdministrationModule');
+    if ($showAdministrationModule == true) {
+        \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+            'Prometheus',
+            'system',
+            'prometheus',
+            '',
+            [
+                \Mfc\Prometheus\Controller\Backend\PrometheusController::class => 'getGrafanaContent',
+            ],
+            [
+                'access' => 'user,group',
+                'icon' => 'EXT:prometheus/Resources/Public/Icons/Icon.svg',
+                'labels' => 'LLL:EXT:prometheus/Resources/Private/Language/locallang_mod.xlf',
+            ]
+        );
+    }
+});
